e79a6c4db7df076ba1968db3c1e42f3ad2537734
iJustinn <ijustinzhong@gmail.com>
2024-09-19 16:52:49 -0400

    data cleaning

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -7,38 +7,37 @@
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
 
+
+
 #### Workspace setup ####
-library(tidyverse)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
+
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+# Read the raw data
+raw_data <- read.csv("data/raw_data/raw_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
+# Separate the TIME_PERIOD column into Year and Month
+cleaned_data <- raw_data %>%
+  separate(TIME_PERIOD, into = c("Year", "Month"), sep = "-") %>%
   mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+    # Combine Year and Month into a Date object
+    DATE = ymd(paste(Year, Month, "01", sep = "-")),
+    # Drop the separate Year and Month columns if you only want the date
+    Year = as.numeric(Year),
+    Month = as.numeric(Month)
+  )
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+
+
+
+#### save data ####
+write_csv(
+  x = cleaned_data,
+  file = "data/analysis_data/analysis_data.csv"
+)
+
+
