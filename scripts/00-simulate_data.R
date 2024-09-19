832d4f1ab71345aa72e65d568f4297c1ba6695aa
iJustinn <ijustinzhong@gmail.com>
2024-09-19 16:50:59 -0400

    data simulation

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -8,12 +8,54 @@
 # Any other information needed? [...UPDATE THIS...]
 
 
+
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
+
+
 
 #### Simulate data ####
+# Set the parameters for simulation
+num_rows <- 1000  # Number of rows to generate
+civic_centers <- c('ET', 'NY', 'SC', 'TO', 'BX')  # List of civic centers
+lambda_value <- 10  # Lambda for the Poisson distribution
+
+# Set a seed for reproducibility
+set.seed(304)
+
+# Generate _id as sequential integers
+simulated_id <- 1:num_rows
+
+# Randomly select civic centers
+simulated_civic_centres <- sample(civic_centers, size = num_rows, replace = TRUE)
+
+# Simulate marriage licenses using Poisson distribution
+set.seed(123)  # Set seed for reproducibility
+simulated_licenses <- rpois(num_rows, lambda_value)
+
+# Generate random dates between 2020-01-01 and 2024-12-31
+start_date <- as.Date("2020-01-01")
+end_date <- as.Date("2024-12-31")
+random_dates <- sample(seq(start_date, end_date, by = "day"), num_rows, replace = TRUE)
+
+# Convert the dates to "YYYY-MM" format
+simulated_time_period <- format(random_dates, "%Y-%m")
+
+# Create the simulated data frame
+simulated_data <- data.frame(
+  id = simulated_id,
+  CIVIC_CENTRE = simulated_civic_centres,
+  MARRIAGE_LICENSES = simulated_licenses,
+  TIME_PERIOD = simulated_time_period
+)
 
 
+
+#### save data ####
+write_csv(
+  x = raw_data,
+  file = "data/simulation_data.csv"
+)
 
 
